643a1e06f617c7c5750e82be9ebc89bc2cac6fdd
Ahmed Alaa <ahmedalaa24683901@gmail.com>
2024-11-06 20:08:02 +0200

    dockerize the first app

--- first-app/firstApp.js
+++ first-app/firstApp.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const app = express();
 
-getMethod = ( req , res ) => {
-    res.status(200).json ({
-        id: "1" ,
+const getMethod = (req, res) => {
+    console.log("Received a GET request on /json");
+    res.status(200).json({
+        id: "1",
         message: "Hello World",
     });
 };
 
 app.route("/json").get(getMethod);
-const PORT = process.env.PORT || 8080;
+
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, err => {
-    console.log(`Server is running on port ${PORT}`);
+    if (err) {
+        console.error("Error starting server:", err);
+    } else {
+        console.log(`Server is running on port ${PORT}`);
+    }
 });
